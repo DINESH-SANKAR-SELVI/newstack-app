ff830269f8a31cd5e98c906d041c6c0a8e6f0cae
DINESH-SANKAR-SELVI <dinesh.sankar0110@gmail.com>
2026-04-08 11:28:24 +0530

    new tech stack setup

---
+++ store/authStore.ts
@@ -0,0 +1,7 @@
+import { create } from 'zustand';
+
+export const useAuthStore = create((set) => ({
+  user: null,
+  setUser: (user: any) => set({ user }),
+  logout: () => set({ user: null })
+}));
